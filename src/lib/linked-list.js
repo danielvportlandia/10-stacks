a0616bb70eb15024d49911dfd16568f20739d6c3
danielvportlandia <danielshelton90@gmail.com>
2018-04-27 22:24:35 -0700

    Lab-10 complete.

---
+++ src/lib/linked-list.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const Node = require('./node');
+
+module.exports = class LinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
+
+  push(value) { // Big O(n)
+    const node = new Node(value);
+
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+      this.length += 1;
+      return this;
+    }
+
+    let currentNode = this.head;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+    currentNode.next = node;
+    this.tail = currentNode.next;
+    this.length += 1;
+
+    return this;
+  }
+
+  pop() { // Big O(n)
+    if (this.length === 0) {
+      return undefined;
+    }
+
+    let currentNode = this.head;
+    if (!currentNode.next) {
+      this.head = null;
+      this.tail = null;
+      this.length -= 1;
+      return currentNode.value;
+    }
+    while (currentNode.next) {
+      if (currentNode.next === this.tail) {
+        this.tail = currentNode;
+        currentNode.next = null;
+        this.length -= 1;
+        return this;
+      }
+      currentNode = currentNode.next;
+    }
+
+    return this;
+  }
+};
---
+++ src/main.js
@@ -0,0 +1,11 @@
+// 'use strict';
+
+// const Stack = require('./lib/stack');
+
+// const testStack = new Stack();
+
+// testStack.push(5);
+// testStack.push(10);
+// testStack.push(99);
+// testStack.peek();
+// console.log('final', testStack.isEmpty());
